0de3a328b14759ef8f2f4cb9c9e471f795fd6ff9
Nick047 <nicomurphy@live.co.uk>
2014-09-23 10:29:35 +0100

    Initialised SDL2
    Initialise SDL2, Create Window and Cleanup

--- NMURPH205-Github/main.cpp
+++ NMURPH205-Github/main.cpp
@@ -1,17 +1,48 @@
-//header
+//Header Files
 #include <iostream>
+#include <SDL.h>		//The header for SDL2 functionality
 
+//Global varibles
+//pointers to our SDL Windows
+SDL_Window*window;
+
+//Constants to control window creation
+const int WINDOW_WIDTH = 640;
+const int WINDOW_HEIGHT = 480;
+
+//Global Functions
 void InitWindow(int width, int height, bool fullscreen)
 {
+	//Creates a window
 
+	window = SDL_CreateWindow(
+		"Lab 1",	//Window Title
+		SDL_WINDOWPOS_CENTERED, //x position, centered
+		SDL_WINDOWPOS_CENTERED, //y position, centered
+		width,	//Width, in pixels
+		height,	//Height, in pixels
+		SDL_WINDOW_OPENGL//flags
+		);
 }
 
 void CleanUp()
 {
-
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 }
 
+
+//Main Method - Entry Point
 int main(int argc, char* args[])
 {
+	if ((SDL_INIT_EVERYTHING) != 0)
+	{
+		std::cout << "ERROR SDL_Init" << SDL_GetError() << std::endl;
+
+		return -1;
+	}
+	CleanUp();
 	return 0;
+
+	InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, false);
 }
